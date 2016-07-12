6b767ee1519381b067e9cfff6093e3584d0c1e5d
songlijun <github@itjh.com.cn>
2016-07-12 22:29:59 +0800

    add log4j

--- apis/video-api.js
+++ apis/video-api.js
@@ -1,8 +1,31 @@
 var pool = require('../utils/db-pooling.js');
 
+var log4js = require('log4js');
+
+log4js.configure({
+  appenders: [{
+      type: 'console'
+    }, //控制台输出
+    {
+      type: 'file', //文件输出
+      filename: 'logs/video.log',
+      maxLogSize: 1024,
+      backups: 3,
+      category: 'video'
+    }
+  ],
+  replaceConsole: true
+
+});
+
+var logger = log4js.getLogger('video');
+logger.setLevel('INFO');
+
 
 // 获取视频集合
-exports.getVideosByType = function (req, res, next) {
+exports.getVideosByType = function(req, res, next) {
+
+  logger.info('getVideosByType');
 
   res.setHeader('content-type', 'application/json;charset=utf-8');
   res.charSet('utf-8');
@@ -13,15 +36,15 @@ exports.getVideosByType = function (req, res, next) {
 
   if (Number(req.params.videoId) != 0) {
 
-      sql += ' AND id < ? '
-      values.push(Number(req.params.videoId))
+    sql += ' AND id < ? '
+    values.push(Number(req.params.videoId))
   }
 
   if (Number(req.params.type) != 0) {
 
-      sql += ' AND type = ? '
+    sql += ' AND type = ? '
 
-      values.push(Number(req.params.type))
+    values.push(Number(req.params.type))
 
   }
 
@@ -33,35 +56,35 @@ exports.getVideosByType = function (req, res, next) {
 
   console.log("q values => " + values);
 
-  pool.query(sql,values, function(err, rows, fields) {
-      if (err){
-          console.log(err);
-          throw err;
-      }
+  pool.query(sql, values, function(err, rows, fields) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
 
-      for (var i = 0; i < rows.length; i++) {
-          rows[i]["id"] = rows[i]["id"]+ '';
-          rows[i]["title"] =  new Buffer(rows[i]["title"], 'base64').toString();
-      }
+    for (var i = 0; i < rows.length; i++) {
+      rows[i]["id"] = rows[i]["id"] + '';
+      rows[i]["title"] = new Buffer(rows[i]["title"], 'base64').toString();
+    }
 
-      var resultJson = {
+    var resultJson = {
 
-          message : '获取 '+ rows.length + '条数据',
-          statusCode : 200,
-          content : rows,
-          request : req.url
+      message: '获取 ' + rows.length + '条数据',
+      statusCode: 200,
+      content: rows,
+      request: req.url
 
-      };
+    };
 
-      res.send(200, resultJson);
-      return next();
+    res.send(200, resultJson);
+    return next();
   });
 
 };
 
 
 // 根据视频id获取详情
-exports.getVideosById = function (req, res, next) {
+exports.getVideosById = function(req, res, next) {
 
   console.log('getVideosById api');
 
@@ -76,35 +99,35 @@ exports.getVideosById = function (req, res, next) {
 
   console.log("q values => " + values);
 
-  pool.query(sql,values, function(err, rows, fields) {
-      if (err){
-          console.log(err);
-          throw err;
-      }
+  pool.query(sql, values, function(err, rows, fields) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
 
 
-          rows[0]["id"] = rows[0]["id"]+ '';
-          rows[0]["title"] =  new Buffer(rows[0]["title"], 'base64').toString();
+    rows[0]["id"] = rows[0]["id"] + '';
+    rows[0]["title"] = new Buffer(rows[0]["title"], 'base64').toString();
 
 
-      var resultJson = {
+    var resultJson = {
 
-          message : '获取数据成功',
-          statusCode : 200,
-          content : rows[0],
-          request : req.url
+      message: '获取数据成功',
+      statusCode: 200,
+      content: rows[0],
+      request: req.url
 
-      };
+    };
 
-      res.send(200, resultJson);
-      return next();
+    res.send(200, resultJson);
+    return next();
   });
 
 };
 
 
 // / 根据Banner视频
-exports.getVideosByBanner = function (req, res, next) {
+exports.getVideosByBanner = function(req, res, next) {
 
   console.log('getVideosByBanner api');
 
@@ -119,77 +142,77 @@ exports.getVideosByBanner = function (req, res, next) {
 
   console.log("q values => " + values);
 
-  pool.query(sql,values, function(err, rows, fields) {
-      if (err){
-          console.log(err);
-          throw err;
+  pool.query(sql, values, function(err, rows, fields) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+
+    for (var i = 0; i < rows.length; i++) {
+      rows[i]["id"] = rows[i]["id"] + '';
+      if (rows[i]["id"] > 76384) {
+        rows[i]["title"] = new Buffer(rows[i]["title"], 'base64').toString();
       }
-
-      for (var i = 0; i < rows.length; i++) {
-          rows[i]["id"] = rows[i]["id"]+ '';
-          if (rows[i]["id"] > 76384) {
-             rows[i]["title"] =  new Buffer(rows[i]["title"], 'base64').toString();
-          }
-      }
+    }
 
 
-      var resultJson = {
+    var resultJson = {
 
-          message : '获取数据成功',
-          statusCode : 200,
-          content : rows,
-          request : req.url
+      message: '获取数据成功',
+      statusCode: 200,
+      content: rows,
+      request: req.url
 
-      };
+    };
 
-      res.send(200, resultJson);
-      return next();
+    res.send(200, resultJson);
+    return next();
   });
 
 };
 // / 根据排行榜视频
 exports.getVideoTaxis = function(req, res, next) {
 
-    console.log('getVideoTaxis api');
+  console.log('getVideoTaxis api');
 
-    res.setHeader('content-type', 'application/json;charset=utf-8');
-    res.charSet('utf-8');
+  res.setHeader('content-type', 'application/json;charset=utf-8');
+  res.charSet('utf-8');
 
-    var sql = 'select v.id,v.title,v.url as videoUrl ,v.pic,v.type,v.createTime as pushTime,(select count(*) from `tb_user_video` tub WHERE tub.`status` = 1 AND tub.userId = 1 AND tub.videoId = v.id) as isCollectStatus FROM `tb_video` AS v JOIN (SELECT ROUND(RAND() * (SELECT MAX(id) FROM `tb_video`)) AS id) AS v1 WHERE v.id >= v1.id ORDER BY v.id ASC LIMIT 25';
+  var sql = 'select v.id,v.title,v.url as videoUrl ,v.pic,v.type,v.createTime as pushTime,(select count(*) from `tb_user_video` tub WHERE tub.`status` = 1 AND tub.userId = 1 AND tub.videoId = v.id) as isCollectStatus FROM `tb_video` AS v JOIN (SELECT ROUND(RAND() * (SELECT MAX(id) FROM `tb_video`)) AS id) AS v1 WHERE v.id >= v1.id ORDER BY v.id ASC LIMIT 25';
 
-    var values = [];
+  var values = [];
 
-    console.log("q sql => " + sql);
+  console.log("q sql => " + sql);
 
-    console.log("q values => " + values);
+  console.log("q values => " + values);
 
-    pool.query(sql, values, function(err, rows, fields) {
-        if (err) {
-            console.log(err);
-            throw err;
-        }
+  pool.query(sql, values, function(err, rows, fields) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
 
-        for (var i = 0; i < rows.length; i++) {
-            rows[i]["id"] = rows[i]["id"] + '';
+    for (var i = 0; i < rows.length; i++) {
+      rows[i]["id"] = rows[i]["id"] + '';
 
-            if (rows[i]["id"] > 76384) {
-                rows[i]["title"] = new Buffer(rows[i]["title"], 'base64').toString();
-            }
+      if (rows[i]["id"] > 76384) {
+        rows[i]["title"] = new Buffer(rows[i]["title"], 'base64').toString();
+      }
 
-        }
+    }
 
 
-        var resultJson = {
+    var resultJson = {
 
-            message: '获取数据成功',
-            statusCode: 200,
-            content: rows,
-            request: req.url
+      message: '获取数据成功',
+      statusCode: 200,
+      content: rows,
+      request: req.url
 
-        };
+    };
 
-        res.send(200, resultJson);
-        return next();
-    });
+    res.send(200, resultJson);
+    return next();
+  });
 
 };
